225f0f9cabf2f10489a723210e035b70945683fc
rainbow82 <shannonbeach1982@gmail.com>
2015-08-23 20:54:17 -0400

    JavaScript Files
    initializing the popovers used to display the interesting facts about
    each show.

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,5 @@
-//hididng the #fact on load
-// when mouse down, hide #pic
-// diplay #fact
-// when mouseup, hide #fact
-// display #pic
+//initialing popovers 
 
- //hide fact
-$(document).ready(function(){
-    $("#fact, #fact span").hide();
-});
-
-// hide #pic on mousedown
-$("#pic").click(function(){
-   $(this).hide().$("#fact, #fact span").show();
-});
-
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
