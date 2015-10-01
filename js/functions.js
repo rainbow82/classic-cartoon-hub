99aacd1aea3d6268b43b03cdd07eeb5b299682f8
rainbow82 <shannonbeach1982@gmail.com>
2015-10-01 18:58:46 -0400

    Updates to mobile

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,23 @@
+// collapse plug in
+$('.collapse').collapse()
+
+
 //initialing popovers 
 
-$(function () {
+$(function() {
   $('[data-toggle="popover"]').popover()
+ 
 })
+
+
+$(document).ready(function() {
+    $("#info_popover").popover({
+        placement: 'bottom',
+        html: 'true',
+        title : '<span class="text-info"><strong>title</strong></span>'+
+                '<button type="button" id="close" class="close" onclick="$(&quot;#info_popover&quot;).popover(&quot;hide&quot;);">&times;</button>',
+        content : 'test'
+    });
+});
+
+
