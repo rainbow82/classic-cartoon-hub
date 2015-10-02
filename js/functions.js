3b33a0c6a187017bf79aef4f10aaf27d20a3375b
rainbow82 <shannonbeach1982@gmail.com>
2015-10-01 20:56:52 -0400

    Updated Interactivity
    Added rotating images to the about page, updated the log in button on
    the home page to be a modal instead of a pop up box

--- js/functions.js
+++ js/functions.js
@@ -21,3 +21,52 @@ $(document).ready(function() {
 });
 
 
+//alert pop up on submit button click
+$( "#submit" ).click(function() {
+  alert( "Thank you for the feedback" );
+});
+
+// log in modal
+
+//Display login in box when login button clicked
+$("#logIn_btn").click(function(){
+	alert("LogIn Test");
+});
+
+ $("#modal_trigger").leanModal({
+	 top : 200, overlay : 0.6, 
+	 closeButton: ".modal_close" 
+});
+
+$(function () {
+    // Calling Login Form
+    $("#login_form").click(function () {
+        $(".social_login").hide();
+        $(".user_login").show();
+        return false;
+    });
+
+    // Calling Register Form
+    $("#register_form").click(function () {
+        $(".social_login").hide();
+        $(".user_register").show();
+        $(".header_title").text('Register');
+        return false;
+    });
+
+    // Going back to Social Forms
+    $(".back_btn").click(function () {
+        $(".user_login").hide();
+        $(".user_register").hide();
+        $(".social_login").show();
+        $(".header_title").text('Login');
+        return false;
+    });
+
+})	
+
+
+ // connect to control panel when login successful
+ function loadCtlPanel() {
+    window.open("controlPanel.html", "_self");
+}
